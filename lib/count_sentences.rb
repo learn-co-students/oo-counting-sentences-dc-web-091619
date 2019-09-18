17028070427ce181d0b710fb7ffaa45902612e35
Adam Finch <atyreefinch@gmail.com>
2019-09-18 07:30:30 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    #regex - to include multiple incidences of the punctuation use "+" after the punctuation
+    arr = self.split(/\.+|\?+|\!+/)
+    arr.length
   end
 end

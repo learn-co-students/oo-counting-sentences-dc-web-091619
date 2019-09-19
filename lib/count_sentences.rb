e13c51d41e8a3a6d54c18f728404e2752b2b92c2
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-09-19 14:51:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
+    self[-1] == '.'
     
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
-  end
+    sentence = self.split(/[?.!]/).select{|frag|
+      frag if frag.length > 1 }
+    sentence.count
+end
 end

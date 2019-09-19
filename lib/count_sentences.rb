55849ef54eec46ab7351d44b38d4add8edcb0b21
christinapark0117 <christinapark0117@gmail.com>
2019-09-19 12:33:36 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #if self[self.length - 1] == '!'
+     # return true
+    #else
+     # return false
+    #end  
+    self[-1] == '.' 
   end
 
   def question?
-
+      self[-1] == '?'
   end
 
   def exclamation?
-
+     self[-1] == '!'
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
